74fcb84b092d5337c59ab5100a5850d51e76876e
Venkatesh Chakrabarty <venkatesh.c@menerva.tech>
2021-04-09 17:13:03 +0530

    [ADD] Add form validation

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,36 +5,48 @@ import { Link } from "react-router-dom";
 const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [noEmail, setNoEmail] = useState(false);
+  const [noPassword, setNoPassword] = useState(false);
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
+    setNoEmail(false);
   };
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
+    setNoPassword(false);
   };
 
-  const handleFormSubmit = () => {};
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (!email) {
+      setNoEmail(true);
+    } else if (!password) {
+      setNoPassword(true);
+    } else {
+    }
+  };
 
   return (
     <div className={"login-form-container"}>
       <div className={"form-banner"}>Already with us? Login now.</div>
       <form className={"login-form"} onSubmit={handleFormSubmit}>
         <input
-          className={"login-input"}
+          className={`login-input ${noEmail ? "no-input" : ""}`}
           type={"email"}
           value={email}
           placeholder={"email"}
           onChange={handleEmailChange}
         />
         <input
-          className={"login-input"}
+          className={`login-input ${noPassword ? "no-input" : ""}`}
           type={"password"}
           value={password}
           placeholder={"password"}
           onChange={handlePasswordChange}
         />
-        <input className={"login-submit"} type={"submit"} />
+        <input className={"login-submit"} type={"submit"} value={"Login"} />
       </form>
       <div className={"to-signup"}>
         <div>New here?&nbsp;</div>
--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -8,28 +8,56 @@ const SignupForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [noFirstName, setNoFirstName] = useState(false);
+  const [noLastName, setNoLastName] = useState(false);
+  const [noEmail, setNoEmail] = useState(false);
+  const [noPassword, setNoPassword] = useState(false);
+  const [noConfirmPassword, setNoConfirmPassword] = useState(false);
+  const [noMatch, setNoMatch] = useState(false);
 
   const handleFirstNameChange = (e) => {
     setFirstName(e.target.value);
+    setNoFirstName(false);
   };
 
   const handleLastNameChange = (e) => {
     setLastName(e.target.value);
+    setNoLastName(false);
   };
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
+    setNoEmail(false);
   };
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
+    setNoPassword(false);
   };
 
   const handleConfirmPasswordChange = (e) => {
     setConfirmPassword(e.target.value);
+    setNoConfirmPassword(false);
   };
 
-  const handleFormSubmit = () => {};
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (!firstName) {
+      setNoFirstName(true);
+    } else if (!lastName) {
+      setNoLastName(true);
+    } else if (!email) {
+      setNoEmail(true);
+    } else if (!password) {
+      setNoPassword(true);
+    } else if (!confirmPassword) {
+      setNoConfirmPassword(true);
+    } else {
+      if (password !== confirmPassword) {
+        setNoMatch(true);
+      }
+    }
+  };
 
   return (
     <div className={"signup-form-container"}>
@@ -39,42 +67,48 @@ const SignupForm = () => {
       </div>
       <form className={"signup-form"} onSubmit={handleFormSubmit}>
         <input
-          className={"signup-input"}
+          className={`signup-input ${noFirstName ? "no-input" : ""}`}
           type={"text"}
           value={firstName}
           placeholder={"first name"}
           onChange={handleFirstNameChange}
         />
         <input
-          className={"signup-input"}
+          className={`signup-input ${noLastName ? "no-input" : ""}`}
           type={"text"}
           value={lastName}
           placeholder={"last name"}
           onChange={handleLastNameChange}
         />
         <input
-          className={"signup-input"}
+          className={`signup-input ${noEmail ? "no-input" : ""}`}
           type={"email"}
           value={email}
           placeholder={"email"}
           onChange={handleEmailChange}
         />
         <input
-          className={"signup-input"}
+          className={`signup-input ${noPassword ? "no-input" : ""}`}
           type={"password"}
           value={password}
           placeholder={"password"}
           onChange={handlePasswordChange}
         />
         <input
-          className={"signup-input"}
+          className={`signup-input ${noConfirmPassword ? "no-input" : ""}`}
           type={"password"}
           value={confirmPassword}
           placeholder={"confirm password"}
           onChange={handleConfirmPasswordChange}
         />
+        {noMatch ? (
+          <div className={"no-match"}>Passwords do not match.</div>
+        ) : (
+          <></>
+        )}
         <input className={"signup-submit"} type={"submit"} value={"Signup"} />
       </form>
+
       <div className={"to-login"}>
         <div>Already a member?&nbsp;</div>
         <Link to={"/login"}>Login</Link>
